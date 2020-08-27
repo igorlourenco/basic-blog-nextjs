84145f25399806c988e2e245435de1615d91fc7e
Igor Lourenço <theigorlourenco@gmail.com>
2020-08-27 14:24:44 -0300

    Scripts do Netlifly CMS

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,18 @@
 import '../styles/globals.css'
 import React from "react";
-import { ThemeProvider } from "@chakra-ui/core";
+import {ThemeProvider} from "@chakra-ui/core";
 
-function MyApp({ Component, pageProps }) {
-  return (
-      <ThemeProvider>
-      <Component {...pageProps} />
-      </ThemeProvider>
-      )
+function MyApp({Component, pageProps}) {
+    return (
+        <>
+            <Head>
+                <script src="https://identity.netlify.com/v1/netlify-identity-widget.js"></script>
+            </Head>
+            <ThemeProvider>
+                <Component {...pageProps} />
+            </ThemeProvider>
+        </>
+    )
 }
 
 export default MyApp
