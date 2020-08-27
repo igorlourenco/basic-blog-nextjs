3fc7a10538cdf6a5f3f4cada240e00f668b14f7a
Igor Lourenço <theigorlourenco@gmail.com>
2020-08-27 13:31:27 -0300

    Finalização - Criação estática de posts

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,30 @@
 import React from "react";
-import { Grid, Link} from "@chakra-ui/core";
+import {Grid, Link} from "@chakra-ui/core";
+import fs from "fs";
 
-export default function Home() {
-  return (
-    <Grid>
-     <Link href="/first-post">
-       primeiro post
-     </Link>
-    </Grid>
-  )
+export default function Home({slugs}) {
+    return (
+        <Grid>
+            {slugs.map( slug => {
+                return (
+                   <Grid key={slug}>
+                       <Link href={"/blog/" + slug}>
+                           {slug}
+                       </Link>
+                   </Grid>
+                )
+            } )}
+        </Grid>
+    )
 }
+
+export const getStaticProps = async () => {
+
+    const files = fs.readdirSync( "posts" )
+
+    return {
+        props: {
+            slugs: files.map( filename => filename.replace( ".md", "" ) )
+        }
+    }
+};
