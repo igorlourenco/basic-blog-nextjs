afc95eb5210be7ff35f0227351d6b1d0f9d133ab
Igor Lourenço <theigorlourenco@gmail.com>
2020-08-27 14:34:30 -0300

    Importação da tag <Head> (correção)

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
 import '../styles/globals.css'
 import React from "react";
 import {ThemeProvider} from "@chakra-ui/core";
+import Head from "next/head";
 
 function MyApp({Component, pageProps}) {
     return (
