efcf8957b2bb9ef7c1647de2f43a03f54cc044d0
Igor <theigorlourenco@gmail.com>
2020-09-02 13:53:06 -0300

    Remoção de arquivos do Chakra UI

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,5 @@
 import '../styles/global.css';
-import customTheme from "../styles/themes";
 import React from "react";
-import {ThemeProvider, CSSReset} from "@chakra-ui/core";
 import Head from "next/head";
 
 function MyApp({Component, pageProps}) {
@@ -10,9 +8,7 @@ function MyApp({Component, pageProps}) {
             <Head>
                 <script src="https://identity.netlify.com/v1/netlify-identity-widget.js"></script>
             </Head>
-            <ThemeProvider theme={customTheme}>
                     <Component {...pageProps} />
-            </ThemeProvider>
         </>
     )
 }
