7dbd1d7823e21e0482456e00819ed176b83ebdb8
Igor <theigorlourenco@gmail.com>
2020-09-02 13:45:58 -0300

    Correção na exibição da página inicial

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,43 @@
-import React from "react";
-import Home from "./components/Home";
+import React, {useEffect} from "react";
 import fs from "fs";
 
 export default function Main({slugs}) {
-   return(
-       <Home slugs={slugs}/>
-   )
+    useEffect( () => {
+        if (window.netlifyIdentity) {
+            window.netlifyIdentity.on( "init", user => {
+                if (!user) {
+                    window.netlifyIdentity.on( "login", () => {
+                        document.location.href = "/admin/";
+                    } );
+                }
+            } );
+        }
+    }, [] )
+
+    return (
+        <>
+            <div>
+                {slugs.map( slug => {
+                    return (
+                        <div key={slug}>
+                            <a href={"/blog/" + slug}>
+                                {slug}
+                            </a>
+                        </div>
+                    )
+                } )}
+            </div>
+        </>
+    )
 }
 
 export const getStaticProps = async () => {
 
-   const files = fs.readdirSync( "posts" )
+    const files = fs.readdirSync( "posts" )
 
-   return {
-      props: {
-         slugs: files.map( filename => filename.replace( ".md", "" ) )
-      }
-   }
+    return {
+        props: {
+            slugs: files.map( filename => filename.replace( ".md", "" ) )
+        }
+    }
 };
